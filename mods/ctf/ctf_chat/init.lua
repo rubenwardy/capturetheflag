52e2c98e96d3008e7d71f46b3d8c91f5de0fb449
_Lucy <80708819+Lucyucy@users.noreply.github.com>
2021-06-28 16:32:32 +0200

    Add team chat (#13)
    * Add team chat
    
    * Update init.lua
    
    Co-authored-by: LoneWolfHT <lonewolf04361@gmail.com>

--- mods/ctf/ctf_chat/init.lua
+++ mods/ctf/ctf_chat/init.lua
@@ -91,3 +91,36 @@ minetest.registered_chatcommands["me"].func = function(name, param)
 
 	minetest.chat_send_all(name .. " " .. param)
 end
+
+minetest.register_chatcommand("t", {
+    params = "msg",
+    description = "Send a message on the team channel",
+    privs = { interact = true, shout = true },
+    func = function(name, param)
+        if param == "" then
+            return false, "-!- Empty team message, see /help t"
+        end
+
+        local tname = ctf_teams.get(name)
+        if tname then
+     	   local team = ctf_teams.get_team(tname)
+
+            minetest.log("action", tname .. "<" .. name .. "> ** ".. param .. " **")
+            if minetest.global_exists("chatplus") then
+                chatplus.log("<" .. name .. "> ** ".. param .. " **")
+            end
+
+            local tcolor = ctf_teams.team[tname].color 
+            for _, username in pairs(team) do
+                minetest.chat_send_player(username,
+                        minetest.colorize(tcolor, "<" .. name .. "> ** " .. param .. " **"))
+            end
+            if minetest.global_exists("irc") and irc.feature_mod_channel then
+                irc:say(irc.config.channel, tname .. "<" .. name .. "> ** " .. param .. " **", true)
+            end
+        else
+            minetest.chat_send_player(name,
+                    "You're not in a team, so you have no team to talk to.")
+        end
+    end
+})
